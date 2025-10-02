1f99e0123e0595f401d8f8602839f9521ac5dd63
kacperfl <kacperfl29@gmail.com>
2025-10-02 16:16:26 +0200

    opdracht 1

---
+++ Variabelen & Types/opdracht1.py
@@ -0,0 +1,12 @@
+# Opdracht:
+
+# Maak twee variabelen: leeftijd en naam.
+
+# Zet jouw leeftijd in leeftijd en jouw naam in naam.
+
+# Print een zin: "Mijn naam is ... en ik ben ... jaar oud." met die variabelen.
+
+leeftijd = 20
+naam = "Kacper"
+
+print(f"Mijn naam is {naam} en ik ben {leeftijd} jaar oud.")
